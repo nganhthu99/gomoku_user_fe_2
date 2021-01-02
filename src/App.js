f40730a9009911d72d678938ca1221cbec560022
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-02 10:27:29 +0700

    Add router to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from "./Components/Home/home";
+import SignIn from "./Components/Authentication/sign-in";
+import SignUp from "./Components/Authentication/sign-up";
+import { Switch, Route } from 'react-router-dom';
+import Game from "./Components/Game/game";
+import Profile from "./Components/Profile/profile";
+import {SocketProvider} from "./Core/Provider/socket-provider";
+import GameHistory from "./Components/GameHistory/game-history";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <SocketProvider>
+        <Switch>
+            <Route path='/history' component={GameHistory}/>
+            <Route path='/profile' component={Profile}/>
+            <Route path='/game' component={Game}/>
+            <Route path='/home' component={Home}/>
+            <Route path='/sign-up' component={SignUp}/>
+            <Route path='/sign-in' component={SignIn}/>
+            <Route path='/' component={SignIn}/>
+        </Switch>
+      </SocketProvider>
   );
 }
 
