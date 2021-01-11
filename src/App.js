71640130837948cfc6d6a8c75784614d38bcea14
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-11 12:24:59 +0700

    Update app.js

--- src/App.js
+++ src/App.js
@@ -7,30 +7,39 @@ import Game from "./Components/Game/game";
 import Profile from "./Components/Profile/profile";
 import {SocketProvider} from "./Core/Provider/socket-provider";
 import GameHistory from "./Components/GameHistory/game-history";
+import Ranking from "./Components/Ranking/ranking";
+import ResultResetPassword from "./Components/Authentication/result-reset-password";
+import {RouteName} from "./Constant/route";
+import ForgetPassword from "./Components/Authentication/forget-password";
+import ResetPassword from "./Components/Authentication/reset-password";
 
 /*
-bug:
-    timer
-    layout
-function:
-    play now
-    invite
-    new game
-    disconnect
-    ranking
-    profile user
+    SOCKET IO
+        play now
+        invite
+        new game
+        disconnect
+        time-out
+
+    API CALL:
+        ranking
+        profile user
 */
 
 function App() {
   return (
       <SocketProvider>
         <Switch>
-            <Route path='/history' component={GameHistory}/>
-            <Route path='/profile' component={Profile}/>
-            <Route path='/game' component={Game}/>
-            <Route path='/home' component={Home}/>
-            <Route path='/sign-up' component={SignUp}/>
-            <Route path='/sign-in' component={SignIn}/>
+            <Route path={RouteName.ForgetPassword} component={ForgetPassword}/>
+            <Route path={RouteName.Ranking} component={Ranking}/>
+            <Route path={RouteName.GameHistory} component={GameHistory}/>
+            <Route path={RouteName.Profile} component={Profile}/>
+            <Route path={RouteName.Game} component={Game}/>
+            <Route path={RouteName.Home} component={Home}/>
+            <Route path={RouteName.SignUp} component={SignUp}/>
+            <Route path={RouteName.SignIn} component={SignIn}/>
+            <Route path={RouteName.ResultResetPassword} component={ResultResetPassword}/>
+            <Route path={RouteName.ResetPassword} component={ResetPassword}/>
             <Route path='/' component={SignIn}/>
         </Switch>
       </SocketProvider>
