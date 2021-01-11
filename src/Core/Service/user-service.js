9ba57002c5bd31016f19d983f0d57254408164c7
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-11 12:21:53 +0700

    Add user service api call

--- src/Core/Service/user-service.js
+++ src/Core/Service/user-service.js
@@ -1,75 +1,47 @@
-// import axios from "axios";
-// import {ENDPOINT} from "../../Constant/ENDPOINT";
-import {BOARD_SIZE} from "../../Components/Game/board";
+import axios from "axios";
+import {ENDPOINT} from "../../Constant/ENDPOINT";
 
-export const getUserInfoService = (username) => {
-    return {
-        username: 'nganhthu',
-        joinedDate: '2020/12/20',
-        rank: 'diamond',
-        winningPercentage: '34%'
-    }
+export const getUserInfo = (username, token) => {
+    return axios.get(ENDPOINT + 'users/user/user-info?name=' + username, {
+        headers: {
+            Authorization: token
+        }
+    })
 }
 
-export const getUserWithGameInfoService = (username) => {
-    return {
-        username: 'nganhthu',
-        joinedDate: 'Fri, Jan 01 2021',
-        rank: 'diamond',
-        winningPercentage: '34%',
-        games: [
-            {
-                _id: '12321',
-                room: 'chicken',
-                playedDate: "Fri, 01 Jan 2021 03:54:24 GMT",
-                turn:{
-                    move_x: "anhthu",
-                    move_o: "duytuan",
-                },
-                history: [
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null)
-                ],
-                messages: [
-                    {
-                        sender: "admin",
-                        message: "anhthu has joined room"
-                    },
-                    {
-                        sender: "admin",
-                        message: "duytuan has joined room"
-                    }
-                ]
-            },
-            {
-                _id: '12351',
-                room: 'duck',
-                playedDate: "Fri, 01 Jan 2021 03:54:24 GMT",
-                turn:{
-                    move_x: "anhthu",
-                    move_o: "duytuan",
-                },
-                history: [
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-                    Array(BOARD_SIZE * BOARD_SIZE).fill(null)
-                ],
-                messages: [
-                    {
-                        sender: "admin",
-                        message: "anhthu has joined room"
-                    },
-                    {
-                        sender: "admin",
-                        message: "duytuan has joined room"
-                    }
-                ]
-            }
-        ]
-    }
+export const getUserByUsername = (username, token) => {
+    console.log('token: ', token)
+    console.log('username: ', username)
+    return axios.get(ENDPOINT + 'users/user?username=' + username, {
+        headers: {
+            Authorization: token
+        }
+    })
+}
+
+export const getUserByDisplayName = (username, token) => {
+    return axios.get(ENDPOINT + 'users/user/display-name?name=' + username, {
+        headers: {
+            Authorization: token
+        }
+    })
+}
+
+export const getUserList = (token) => {
+    return axios.get(ENDPOINT + 'users/leaderboard', {
+        headers: {
+            Authorization: token
+        }
+    })
+}
+
+export const updateUserAvatar = (username, imageUrl, token) => {
+    return axios.put(ENDPOINT + 'users/update', {
+        username,
+        avatar: imageUrl
+    }, {
+        headers: {
+            Authorization: token
+        }
+    })
 }
