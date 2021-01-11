4ef2dc363ac79b0b1a72e043dafed3d3e929eca3
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-11 12:23:00 +0700

    Update authentication service api call

--- src/Core/Service/authentication-service.js
+++ src/Core/Service/authentication-service.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 import {ENDPOINT} from "../../Constant/ENDPOINT";
 
-export const signUpService = (username, email, password) => {
-    return axios.post(ENDPOINT + 'user/register', {
+export const signUpService = (username, password, displayName) => {
+    return axios.post(ENDPOINT + 'users/signup', {
         username,
+        password,
+        displayName
+    }, {
+        validateStatus: () => true
+    })
+}
+
+export const signUpSocialAccountService = (id, email, displayName) => {
+    return axios.post(ENDPOINT + 'users/signup', {
+        username: id,
         email,
-        password
+        displayName
+    }, {
+        validateStatus: () => true
     })
 }
 
@@ -13,5 +25,30 @@ export const signInService = (username, password) => {
     return axios.post(ENDPOINT + 'users/signin', {
         username,
         password
+    })
+}
+
+export const checkUsernameEmailService = (id, email) => {
+    return axios.post(ENDPOINT + 'users/check', {
+        username: id,
+        email
+    }, {
+        validateStatus: () => true
+    })
+}
+
+export const requestVerificationService = (username) => {
+    return axios.post(ENDPOINT + 'users/request-password-reset', {
+        username
+    }, {
+        validateStatus: () => true
+    })
+}
+
+export const resetPasswordService = (token, password) => {
+    return axios.post(ENDPOINT + 'users/reset-password?token=' + token, {
+        newPassword: password
+    }, {
+        validateStatus: () => true
     })
 }
