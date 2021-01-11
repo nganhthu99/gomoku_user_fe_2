41b2eb560615ca4f5d52433e69cf8eed466d0054
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-11 12:21:24 +0700

    Add get game by id api call service

--- src/Core/Service/game-service.js
+++ src/Core/Service/game-service.js
@@ -1,45 +1,10 @@
-import {BOARD_SIZE} from "../../Components/Game/board";
+import axios from "axios";
+import {ENDPOINT} from "../../Constant/ENDPOINT";
 
-export const getGameHistoryService = (gameId) => {
-    return {
-        _id: '12321',
-        room: 'chicken',
-        playedDate: "Fri, 01 Jan 2021 03:54:24 GMT",
-        turn:{
-            move_x: "anhthu",
-            move_o: "duytuan",
-        },
-        history: [
-            Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-            [
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, 'X', null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, 'O', null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-            Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-            Array(BOARD_SIZE * BOARD_SIZE).fill(null),
-            Array(BOARD_SIZE * BOARD_SIZE).fill(null)
-        ],
-        messages: [
-            {
-                sender: "admin",
-                message: "anhthu has joined room"
-            },
-            {
-                sender: "admin",
-                message: "duytuan has joined room"
-            }
-        ]
-    }
+export const getGameService = (gameId, token) => {
+    return axios.get(ENDPOINT + 'users/user/game?id=' + gameId, {
+        headers: {
+            Authorization: token
+        }
+    })
 }
