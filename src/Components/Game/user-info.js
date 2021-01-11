b0017ba25e75ad4e31e1bbad61641d5f77e0db71
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-11 09:51:32 +0700

    Add api call to user info component

--- src/Components/Game/user-info.js
+++ src/Components/Game/user-info.js
@@ -1,41 +1,62 @@
 import React, {useEffect, useState} from 'react';
-import {Button, OverlayTrigger, Popover} from "react-bootstrap";
-import {getUserInfoService} from "../../Core/Service/user-service";
+import {Button, Container, Image, OverlayTrigger, Popover, Spinner} from "react-bootstrap";
 import { FaMedal } from "react-icons/fa"; // gold
 import { RiMedalFill } from "react-icons/ri";
 import { BiMedal } from "react-icons/bi"; // silver
 import { GiCutDiamond } from "react-icons/gi";
+import {getUserInfo} from "../../Core/Service/user-service";
+import {GiTrophyCup} from "react-icons/gi";
 
 const UserInfo = (props) => {
-    const [userInfo, setUserInfo] = useState({})
+    const [userInfo, setUserInfo] = useState(null)
 
     useEffect(() => {
         // getUserByUsernameService(props.player)
         //     .then((response) => {
         //         setUserInfo(response)
         //     })
-        setUserInfo(getUserInfoService(props.player))
+        // setUserInfo(getUserInfoService(props.player))
+        const token = localStorage.getItem('token')
+        getUserInfo(props.player, token)
+            .then((response) => {
+                console.log('DASDSADAS: ', response.data)
+                if (response.status === 200) {
+                    setUserInfo(response.data.user[0])
+                }
+            })
     }, [props.player])
 
-    const popOver = (
+    const popOver = (userInfo) ? (
         <Popover>
             <Popover.Title as="h3" style={{textAlign: 'center'}}>{`@${props.player}`}</Popover.Title>
+            <Popover.Content>
+                <Image src={userInfo.avatar} style={{width: 150, height: 150, display: 'flex', alignSelf: 'center'}}/>
+            </Popover.Content>
             <Popover.Content style={{textAlign: 'center', padding: 5}}>
                 Rank:
-                {userInfo.rank === 'diamond' && <GiCutDiamond color='#00E4FF' size={36}/>}
-                {userInfo.rank === 'gold' && <FaMedal color='#FFAF03' size={36}/>}
-                {userInfo.rank === 'silver' && <RiMedalFill color='gray' size={36}/>}
-                {userInfo.rank === 'bronze' && <BiMedal color='#562A03' size={36}/>}
+                {userInfo.level === 'diamond' && <GiCutDiamond color='#00E4FF' size={36}/>}
+                {userInfo.level === 'gold' && <FaMedal color='#FFAF03' size={36}/>}
+                {userInfo.level === 'silver' && <RiMedalFill color='gray' size={36}/>}
+                {userInfo.level === 'bronze' && <BiMedal color='#562A03' size={36}/>}
             </Popover.Content>
-            <Popover.Content>
-                Winning Percentage: <strong>{userInfo.winningPercentage}</strong>
+            <Popover.Content style={{textAlign: 'center', padding: 5}}>
+                Trophies: {userInfo.cups}<GiTrophyCup/>
+            </Popover.Content>
+            <Popover.Content style={{textAlign: 'center', padding: 5}}>
+                Winning: {userInfo.wins} matches
             </Popover.Content>
         </Popover>
+    ) : (
+        <Container style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Spinner animation="border" variant='primary'/>
+        </Container>
     )
+
     return (
         <>
             <OverlayTrigger trigger="click" placement='bottom' overlay={popOver}>
-                <Button variant={props.color === 'red' ? "outline-danger" : 'outline-success'}>{`@${props.player}`}</Button>
+                <Button
+                    variant={props.color === 'red' ? "outline-danger" : 'outline-success'}>{`@${props.player}`}</Button>
             </OverlayTrigger>
         </>
     )
