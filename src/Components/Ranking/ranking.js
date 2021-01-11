9dab26e86e4c675dbce3906e864d2c7e4c35f887
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-11 09:50:14 +0700

    Add api call and update ui of ranking screen

--- src/Components/Ranking/ranking.js
+++ src/Components/Ranking/ranking.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Card, Navbar, Row, Col} from "react-bootstrap";
+import {Button, Card, Navbar, Row, Col, Container, Spinner} from "react-bootstrap";
 import randomColor from "randomcolor";
 import {GiTrophyCup} from "react-icons/gi";
 import {useHistory} from "react-router-dom";
 import {RouteName} from "../../Constant/route";
+import {getUserList} from "../../Core/Service/user-service";
 
 const RankingItem = (props) => {
     const random_color = randomColor({
@@ -26,14 +27,14 @@ const RankingItem = (props) => {
             <Col xs={6}>
                 <Card style={{backgroundColor: random_color, margin: 5}}>
                     <Card.Body style={{padding: 5, backgroundColor: random_color}}>
-                        <Button variant='clear' style={{color: 'white', fontWeight: 'bold'}} onClick={handleOnClick}>{`@${props.item.username}`}</Button>
+                        <Button variant='clear' style={{color: 'white', fontWeight: 'bold'}} onClick={handleOnClick}>{`@${props.item.displayName}`}</Button>
                     </Card.Body>
                 </Card>
             </Col>
             <Col xs={3} md={2}>
                 <Card style={{backgroundColor: random_color, margin: 5}}>
                     <Card.Body style={{padding: 5, backgroundColor: random_color, display: 'flex', justifyContent: 'center'}}>
-                        <Button variant='clear' style={{color: 'white', fontWeight: 'bold'}} onClick={handleOnClick}>{`${props.item.trophies}`} <GiTrophyCup/></Button>
+                        <Button variant='clear' style={{color: 'white', fontWeight: 'bold'}} onClick={handleOnClick}>{`${props.item.cups}`} <GiTrophyCup/></Button>
                     </Card.Body>
                 </Card>
             </Col>
@@ -43,51 +44,16 @@ const RankingItem = (props) => {
 
 const Ranking = (props) => {
     const history = useHistory()
-    const [listUsers, setListUsers] = useState([
-        {
-            username: 'nganhthu',
-            trophies: 9,
-        },
-        {
-            username: 'duytuan',
-            trophies: 2,
-        },
-        {
-            username: 'nganhthu',
-            trophies: 9,
-        },
-        {
-            username: 'duytuan',
-            trophies: 2,
-        },
-        {
-            username: 'nganhthu',
-            trophies: 9,
-        },
-        {
-            username: 'duytuan',
-            trophies: 2,
-        },
-        {
-            username: 'nganhthu',
-            trophies: 9,
-        },
-        {
-            username: 'duytuan',
-            trophies: 2,
-        },
-        {
-            username: 'nganhthu',
-            trophies: 9,
-        },
-        {
-            username: 'duytuan',
-            trophies: 2,
-        }
-    ])
+    const [listUsers, setListUsers] = useState(null)
 
     useEffect(() => {
-
+        const token = localStorage.getItem('token')
+        getUserList(token)
+            .then((response) => {
+                if (response.status === 200) {
+                    setListUsers(response.data.users)
+                }
+            })
     }, [])
 
     const handleOnClickPlayerProfile = (user) => {
@@ -97,20 +63,29 @@ const Ranking = (props) => {
         })
     }
 
-    return (
-        <div>
-            <Navbar style={{backgroundColor: '#E5F3FC'}}>
-                <Navbar.Brand style={{color: '#153FF2', fontWeight: 'bold', flexGrow: 1}}>
-                    GOMOKU
-                </Navbar.Brand>
-            </Navbar>
-            <Row style={{justifyContent: 'center'}}>
-                <Col xs={12}>
-                    {listUsers.map((item, index) => <RankingItem item={item} index={index} handleOnClick={handleOnClickPlayerProfile}/>)}
-                </Col>
-            </Row>
-        </div>
-    )
+    if (listUsers) {
+        return (
+            <div>
+                <Navbar style={{backgroundColor: '#E5F3FC'}}>
+                    <Navbar.Brand style={{color: '#153FF2', fontWeight: 'bold', flexGrow: 1}}>
+                        GOMOKU
+                    </Navbar.Brand>
+                </Navbar>
+                <Row style={{justifyContent: 'center'}}>
+                    <h5 style={{flexGrow: 1, fontWeight: 'bold', color: '#153FF2', textAlign: 'center', paddingTop: 5}}>Ranking Chart</h5>
+                    <Col xs={12}>
+                        {listUsers.map((item, index) => <RankingItem item={item} index={index} handleOnClick={handleOnClickPlayerProfile}/>)}
+                    </Col>
+                </Row>
+            </div>
+        )
+    } else {
+        return (
+            <Container style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Spinner animation="border" variant='primary'/>
+            </Container>
+        )
+    }
 };
 
 export default Ranking;
