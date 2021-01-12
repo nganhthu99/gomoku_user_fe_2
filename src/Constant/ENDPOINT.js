edc7c887713ac1d1f38544cdee969ebc1e98d92a
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-12 11:45:24 +0700

    Change ENDPOINT api url

--- src/Constant/ENDPOINT.js
+++ src/Constant/ENDPOINT.js
@@ -1,5 +1,5 @@
 // export const ENDPOINT = 'http://localhost:5034/'
 
-// export const ENDPOINT = 'https://caro-user-api-2.herokuapp.com/'
+export const ENDPOINT = 'https://caro-user-api-2.herokuapp.com/'
 
-export const ENDPOINT = 'https://gomoku-user-be.herokuapp.com/'
+// export const ENDPOINT = 'https://gomoku-user-be.herokuapp.com/'
