000128556ae0afd16a6512f86111dc74c901ecbd
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2021-01-12 10:33:35 +0700

    Update result reset password/activate account screen

--- src/Components/Authentication/result-reset-password.js
+++ src/Components/Authentication/result-reset-password.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Card, Col, Row} from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import {RouteName} from "../../Constant/route";
+import queryString from "query-string";
 
 const ResultResetPassword = (props) => {
     const history = useHistory()
+    const [result, ] = useState(queryString.parse(props.location.search).result)
+    const [message, setMessage] = useState('')
     const handleGoToSignInButton = () => {
         history.replace(RouteName.SignIn)
     }
+
+    useEffect(() => {
+        if (result === 'success') {
+            setMessage('Reset password successfully')
+        } else if (result === 'error') {
+            setMessage('Error resetting password')
+        }
+    }, [result])
 
     return (
         <Row style={{justifyContent: 'center'}}>
             <Col md={6} sx={12}>
                 <Card style={{padding: 20, margin: 10, borderColor: '#153FF2', borderWidth: 1}}>
                     <Card.Title style={{textAlign: 'center'}}>
-                        Reset password successfully
+                        {message}
                     </Card.Title>
                     <Row style={{justifyContent: 'center', margin: 5}}>
                         <Button
                             style={{fontWeight:'bold', width: 220}}
                             onClick={handleGoToSignInButton}>
-                            Sign In
+                            Go back to Sign In
                         </Button>
                     </Row>
                 </Card>
--- src/Components/Profile/result-activate-account.js
+++ src/Components/Profile/result-activate-account.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Card, Col, Row} from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import {RouteName} from "../../Constant/route";
+import queryString from "query-string";
 
 const ResultActivateAccount = (props) => {
     const history = useHistory()
+    const [result, ] = useState(queryString.parse(props.location.search).result)
+    const [message, setMessage] = useState('')
     const handleGoToHomeButton = () => {
         history.replace(RouteName.Home)
     }
+
+    useEffect(() => {
+        if (result === 'success') {
+            setMessage('Verify email successfully')
+        } else if (result === 'error') {
+            setMessage('Error verifying email')
+        }
+    }, [result])
 
     return (
         <Row style={{justifyContent: 'center'}}>
             <Col md={6} sx={12}>
                 <Card style={{padding: 20, margin: 10, borderColor: '#153FF2', borderWidth: 1}}>
                     <Card.Title style={{textAlign: 'center'}}>
-                        Verify email successfully
+                        {message}
                     </Card.Title>
                     <Row style={{justifyContent: 'center', margin: 5}}>
                         <Button
